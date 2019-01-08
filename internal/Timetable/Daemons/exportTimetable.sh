b69e2514e9c8689d4b6ae053f34b010626d700b7
Varun Sethu <varun.sethu1@education.nsw.gov.au>
2019-01-09 00:32:51 +1100

    improved structure

--- internal/Timetable/Daemons/exportTimetable.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -v "https://web3.northsydbo-h.schools.nsw.edu.au/classery/public/api/export/timetable" --ntlm -u skedular:chickenfarm -H 'X-AUTH: !te5D?DI<c0#t=2nZir0_eC4.(`i1>p/xEj[Qk_v10dF|G~*{zvwcwTw+`MS&o)M' > Timetables.json
