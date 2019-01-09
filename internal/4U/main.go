5ae651ffdd0bdb6acc27e896f2e72b3a13a73a84
ShunyaoLiang <thomas.s.liang@gmail.com>
2019-01-09 12:20:43 +1100

    Tweaked FourU.ScanFrom
    Changed comments to more descriptive of the function and removed
    unecessary copying. Function now reads directly into the current object
    instead of using temporary variables.

--- internal/4U/main.go
+++ internal/4U/main.go
@@ -14,39 +14,25 @@ type Article struct {
 	Picture            multipart.File
 	PictureHeader      *multipart.FileHeader
 	ArticleImageUrl    string
-	Link          	   string
+	Link               string
 	TypePost		   string
 }
 
-
-
-// Function merely takes a row and scans it into an article, minimal documentation required
+// Parses an SQL query result into the current Article object
 func (article *Article) ScanFrom(rows *sql.Rows) {
-	var articleID			int64
-	var articleName 		string
-	var articleDesc 		string
-	var articlePublishDate 	time.Time
-	var articleImageUrl 	string
-	var issueLink 			string
-	var typeReq				string
-
-	rows.Scan(&articleID, &articleName, &articleDesc, &articlePublishDate, &articleImageUrl, &issueLink, &typeReq)
-
-	// Insert the values now
-	article.ArticleID  			= 	articleID
-	article.ArticleName 		=   articleName
-	article.ArticleDesc  		=   articleDesc
-	article.ArticleImageUrl 	= 	articleImageUrl
-	article.ArticlePublishDate  = 	articlePublishDate
-	article.Link       	 		=	issueLink
-	article.TypePost		 	=	typeReq
+	rows.Scan(&article.ArticleID,
+	          &article.ArticleName,
+	          &article.ArticleDesc,
+	          &article.ArticlePublishDate,
+	          &article.ArticleImageUrl,
+	          &article.Link,
+	          &article.TypePost)
 }
 
-
 type FourUArticle struct {
-	ArticleID		int64
-	ArticleParentID	int64
-	ArticlePage		int64
-	ArticleName		string
-	ArticleDesc		string
+	ArticleID       int64
+	ArticleParentID int64
+	ArticlePage     int64
+	ArticleName     string
+	ArticleDesc     string
 }
